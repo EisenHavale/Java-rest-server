d34ed2b379a3d5658b7230a7522cb7f7b6a9b207
Eisen Havale <eisenprogramando@gmail.com>
2022-07-20 19:14:06 -0500

    Created user and product constants every one inside the right directoy

---
+++ src/main/java/com/eisenhavale/restserver/models/constants/ProductConstants.java
@@ -0,0 +1,20 @@
+package com.eisenhavale.restserver.models.constants;
+
+public class ProductConstants {
+    public static class ProductTable {
+      static final String TABLE_NAME="products";
+      public static class Id {
+        static final String COLUMN_NAME="id";
+      }
+      public static class Name {
+        static final String COLUMN_NAME="name";
+      }
+      public static class Description {
+        static final String COLUMN_NAME="description";
+      }
+      public static class Price {
+        static final String COLUMN_NAME="price";
+      }
+    }
+
+}
